183b3c129f614b131f73bddd7b49f88db9da5791
Kate <kate.vinnik393@gmail.com>
2020-02-25 21:55:18 +0300

    Added final for non-changing variables, moved generating random array into separate method, added new print format, started handle wrong number format

--- src/com/vinnik/kate/Main.java
+++ src/com/vinnik/kate/Main.java
@@ -4,10 +4,15 @@ package com.vinnik.kate;
 public class Main {
     public static void main(String[] args) {
         try {
-            int amountOfThreads = Integer.parseInt(args[0]);
+            final int amountOfThreads = Integer.parseInt(args[0]);
+            if (amountOfThreads < 0) {
+                System.out.println("Wrong number format. Please, enter positive integer number. ");
+            }
             MultithreadingSorter.measureTimeOfSorting(amountOfThreads);
         } catch (java.lang.ArrayIndexOutOfBoundsException e) {
             System.out.println("Please, enter amount of threads.");
+        } catch (java.lang.NumberFormatException e) {
+            System.out.println("Wrong number format. Please, enter positive integer number. ");
         }
     }
 }
--- src/com/vinnik/kate/MultithreadingSorter.java
+++ src/com/vinnik/kate/MultithreadingSorter.java
@@ -5,9 +5,11 @@ import java.util.concurrent.TimeUnit;
 
 /** Class, which creates given number of threads, and measures time they need to sort random array. */
 public class MultithreadingSorter {
+    private static final int arraySize = 500;
+    private static final int upperBoundForRandom = 10000;
 
     /** Creates given number of threads and executes them. */
-    public static void measureTimeOfSorting(int amountOfThreads) {
+    public static void measureTimeOfSorting(final int amountOfThreads) {
         for (int i = 0; i < amountOfThreads; i++) {
             ThreadSort thread = new ThreadSort(i);
             thread.start();
@@ -15,18 +17,23 @@ public class MultithreadingSorter {
     }
 
 
-    private static class ThreadSort extends Thread {
+    protected static class ThreadSort extends Thread {
         private int[] numbersToSort;
-        private int key;
+        private int identifier;
 
         protected ThreadSort(int number) {
-            numbersToSort = new int[500];
-            key = number;
+            this.numbersToSort = this.generateRandomArray();
+            this.identifier = number;
 
-            for (int i = 0; i < numbersToSort.length; i++) {
-                Random random = new Random();
-                numbersToSort[i] = random.nextInt(10000);
+        }
+
+        private int[] generateRandomArray() {
+            int[] randomArray = new int[arraySize];
+            final Random random = new Random();
+            for (int i = 0; i < randomArray.length; i++) {
+                randomArray[i] = random.nextInt(upperBoundForRandom);
             }
+            return randomArray;
         }
 
         public void run() {
@@ -34,11 +41,11 @@ public class MultithreadingSorter {
             sort();
             long elapsedTimeInNanoseconds = System.nanoTime() - startTime;
             long elapsedTimeInMilliseconds = TimeUnit.MILLISECONDS.convert(elapsedTimeInNanoseconds, TimeUnit.NANOSECONDS);
-            System.out.println(key + " - " + elapsedTimeInMilliseconds);
+            System.out.println("Thread " + identifier + " processed data for " + elapsedTimeInMilliseconds + " milliseconds");
         }
 
         protected void sort() {
-            int length = numbersToSort.length;
+            final int length = numbersToSort.length;
 
             for (int i = 0; i < length - 1; i++) {
                 for (int j = 0; j < length - i - 1; j++) {
