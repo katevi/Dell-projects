358886d84c8285a50162aa76663b2aa66f99c841
Kate <kate.vinnik393@gmail.com>
2020-02-27 13:23:49 +0300

    Created constant for minimal number of threads

--- src/com/vinnik/kate/Main.java
+++ src/com/vinnik/kate/Main.java
@@ -2,10 +2,11 @@ package com.vinnik.kate;
 
 /** Class, which implements interaction with user. */
 public class Main {
+    private final static int MINIMAL_AMOUNT_OF_THREADS = 0;
     public static void main(String[] args) {
         try {
             final int amountOfThreads = Integer.parseInt(args[0]);
-            if (amountOfThreads < 0) {
+            if (amountOfThreads < MINIMAL_AMOUNT_OF_THREADS) {
                 System.out.println("Wrong number format. Please, enter positive integer number. ");
             }
             MultithreadingSorter.measureTimeOfSorting(amountOfThreads);
